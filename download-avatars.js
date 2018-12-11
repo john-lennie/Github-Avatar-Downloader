63187641c69c0c19a030219b1fdf7fbc4c55f0f2
John Lennie <johnlennie@Johns-MacBook-Pro.local>
2018-12-11 12:20:05 -0500

    modified to take owner and name parameters as command line arguments

--- download-avatars.js
+++ download-avatars.js
@@ -2,11 +2,19 @@ var request = require('request');
 var fs = require('fs');
 var token = require('./secrets');
 
+var repoOwner = process.argv[2],
+    repoName = process.argv[3];
+
+if (!repoOwner || !repoName) {
+  console.log("error, 2 arguments needed, i.e. node download_avatars.js <owner> <repo>");
+  return
+};
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(owner, name, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: "https://api.github.com/repos/" + owner + "/" + name + "/contributors",
     headers: {
       'User-Agent': 'john-lennie',
       'Authorization': token.GITHUB_TOKEN
@@ -21,7 +29,7 @@ function downloadImageByURL(url, filePath) {
   request(url).pipe(fs.createWriteStream(filePath))
 }
 
-getRepoContributors("jquery", "jquery", function(err, results) {
+getRepoContributors(repoOwner, repoName, function(err, results) {
   console.log("Errors:", err);
   var resultsInJson = JSON.parse(results); // array of contributors
   resultsInJson.forEach(function(currentValue) {
@@ -29,6 +37,6 @@ getRepoContributors("jquery", "jquery", function(err, results) {
     console.log(url);
     var filePath = "./avatars/" + currentValue.login + ".jpg";
     console.log(filePath);
-    downloadImageByURL(url, filePath);
+    // downloadImageByURL(url, filePath);
   });
 });
